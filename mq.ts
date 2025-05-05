7fd5496b9426002904b1111d537659919e9d8c31
liumingjue <mingjue.liu@icloud.com>
2025-05-05 20:16:02 +0800

    Change the way for handle error

--- mq.ts
+++ mq.ts
@@ -8,16 +8,22 @@ interface RabbitMqInitializer {
     assertQueue(exchange: string, routingKey: string, queue: string): Promise<void>;
 }
 
-export interface SubscriberMessage<T> {
+export interface SubscriberMessage<T extends object> {
     exchange: string;
     routingKey: string;
-    queue: string;
     sourceData: object;
     data: T;
     ackMessage: () => void;
+    throwError: (err: Error) => void;
 }
 
-export type ErrorHandler = <T>(error: Error, message: SubscriberMessage<T>) => void;
+export interface ErrMessage<T extends object> extends SubscriberMessage<T> {
+    errMessage: string;
+    errType: string;
+    sourceExchange: string;
+    sourceRoutingKey: string;
+    replyData: () => void;
+}
 
 export async function connect(onConnected: (init: RabbitMqInitializer) => Promise<void>) {
     const connection = await amqp.connect(env.MQ_HOST);
@@ -35,13 +41,13 @@ export async function connect(onConnected: (init: RabbitMqInitializer) => Promis
 
     await onConnected({ assertExchange, assertQueue });
 
-    function publish(exchange: string, routingKey: string, data: object, priority?: number) {
+    async function publish(exchange: string, routingKey: string, data: object, priority?: number) {
         const json = JSON.stringify(data);
         const buffer = Buffer.from(json, "utf-8");
-        return publisher.publish(exchange, routingKey, buffer, { priority });
+        publisher.publish(exchange, routingKey, buffer, { priority });
     }
 
-    function consume<T>(queue: string, prefetch: number = 200) {
+    function consume<T extends object>(queue: string, prefetch: number = 200) {
         const message$ = new Subject<SubscriberMessage<T>>();
 
         !async function () {
@@ -55,13 +61,25 @@ export async function connect(onConnected: (init: RabbitMqInitializer) => Promis
                 const data = sourceData as T;
                 const ackMessage = () => listener.ack(message);
 
+                async function throwError(err: Error) {
+                    const errData = {
+                        sourceExchange: exchange,
+                        sourceRoutingKey: routingKey,
+                        sourceData,
+                        errType: err.constructor.name,
+                        errMessage: err.message,
+                    }
+                    await assertQueue("error", errData.errType, "errors");
+                    await publish("error", errData.errType, errData);
+                }
+
                 message$.next({
                     exchange,
                     routingKey,
-                    queue,
                     sourceData,
                     data,
                     ackMessage,
+                    throwError,
                 });
             });
         }();
@@ -69,39 +87,47 @@ export async function connect(onConnected: (init: RabbitMqInitializer) => Promis
         return message$;
     }
 
-    const errorHandlers = {
-        reply(): ErrorHandler {
-            return function (_error, message) {
-                publish(message.exchange, message.routingKey, message.sourceData);
-            }
-        },
-        forward(exchange: string, routingKey: string): ErrorHandler {
-            return function (_error, message) {
-                publish(exchange, routingKey, message.sourceData);
-            }
-        },
+    function consumeErrors<T extends object>(prefetch: number = 200) {
+        const errMessage$ = new Subject<ErrMessage<T>>();
+
+        !async function () {
+            const listener = await connection.createChannel();
+            await listener.prefetch(prefetch);
+            await listener.consume("errors", function (message) {
+                if (!message) return;
+                const errMessage = JSON.parse(message.content.toString("utf-8")) as ErrMessage<T>;
+                errMessage.exchange = message.fields.exchange;
+                errMessage.routingKey = message.fields.routingKey;
+                errMessage.data = errMessage.sourceData as T;
+                errMessage.ackMessage = () => listener.ack(message);
+                errMessage.replyData = () =>
+                    publish(errMessage.sourceExchange, errMessage.sourceRoutingKey, errMessage.data as object, 10);
+                errMessage$.next(errMessage);
+            });
+        }();
+
+        return errMessage$;
     }
 
-    return { publish, consume, errorHandlers };
+    return { publish, consume, consumeErrors };
 }
 
-export function filterOrAck<T>(filterByMessageData: (data: T) => boolean) {
-    return filter<SubscriberMessage<T>>((message) =>
-        filterByMessageData(message.data)? true : (message.ackMessage(), false)
-    );
-}
-
-export function attemptData<T>(schema: Record<string, Joi.AnySchema>, errorHandler?: ErrorHandler) {
+export function assertData<T extends object>(schema: Record<string, Joi.AnySchema>) {
     return filter<SubscriberMessage<T>>(function (message) {
         try {
             message.data = Joi.attempt(message.data, Joi.object(schema), { allowUnknown: true });
             return true;
         } catch (error) {
             message.ackMessage();
-            if (errorHandler) {
-                errorHandler(error, message);
-            }
+            message.throwError(error);
             return false;
         }
     });
 }
+
+export function filterOrAck<T extends object>(filterByMessageData: (data: T) => boolean) {
+    return filter<SubscriberMessage<T>>((message) =>
+        filterByMessageData(message.data)? true : (message.ackMessage(), false)
+    );
+}
+
